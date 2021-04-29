3d366ed54e3a22c9fad396c68a3776baac517830
KmecsLilla <kmecs.lilla@gmail.com>
2021-04-29 10:29:17 +0200

    Implement addDigits method

--- src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
+++ src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
@@ -1,5 +1,15 @@
 package hu.nive.ujratervezes.adddigits;
 
 public class AddDigits {
-    //todo: implement your solution here
+    public int addDigits(String input) {
+        if (input == null || input.equals("")) {
+            return -1;
+        }
+        String numbered = input.replaceAll("[^0-9]", "");
+        int result = 0;
+        for (int i = 0; i < numbered.length(); i++) {
+            result += numbered.charAt(i) - '0';
+        }
+        return result;
+    }
 }
